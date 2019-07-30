ec75cec93391621b55a5ce5fa325c673c840fdfa
ChuVo <cahek764@ya.ru>
2019-07-30 22:51:17 +0300

    added function validation

--- src/templates/section-remsmed/section-remsmed.js
+++ src/templates/section-remsmed/section-remsmed.js
@@ -2,7 +2,6 @@
   const pulseBox = document.querySelector('.pulse'),
         widthBox = pulseBox.offsetWidth;
 
-  console.log(widthBox);
   pulseBox.style.height = pulseBox.offsetWidth + "px";
 
 }());
